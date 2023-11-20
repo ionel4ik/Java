12a9a8b47585e41d72967de8931f3791da75de6c
ionel4ik <94807973+ionel4ik@users.noreply.github.com>
2023-11-20 15:27:28 +0200

    Variant4.java

--- src/main/java/org/example/Variant4.java
+++ src/main/java/org/example/Variant4.java
@@ -46,7 +46,7 @@ public class Variant4 {
             case 1, 3, 5, 7, 8, 10, 12 -> 31;
             case 2 -> 28;
             case 4, 6, 9, 11 -> 30;
-            default -> 29;
+            default -> throw new IllegalArgumentException("Wrong month!");
         };
     }
 
